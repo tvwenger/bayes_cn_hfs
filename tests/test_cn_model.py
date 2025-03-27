6dd4b63f2f98624c790ec63f82c487c7ed4df7eb
Trey Wenger <tvwenger@gmail.com>
2025-03-27 14:52:21 -0500

    update tests

--- tests/test_cn_model.py
+++ tests/test_cn_model.py
@@ -21,12 +21,17 @@ def test_cn_model():
     freq_axis = np.linspace(113470.0, 113530.0, 1000)
     brightness = np.random.randn(1000)
     data = {"observation": SpecData(freq_axis, brightness, 1.0)}
-    model = CNModel(data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1)
+    model = CNModel(
+        data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1
+    )
     assert isinstance(model.mol_data, dict)
     with pytest.raises(ValueError):
         model.add_priors(assume_LTE=True, assume_CTEX=False)
     model.add_priors(
-        prior_baseline_coeffs={"observation": [1.0, 1.0]}, prior_rms={"observation": 1.0}, prior_fwhm_nonthermal=1.0
+        prior_baseline_coeffs={"observation": [1.0, 1.0]},
+        prior_rms={"observation": 1.0},
+        prior_fwhm_nonthermal=1.0,
+        prior_fwhm_L=1.0,
     )
     model.add_likelihood()
     assert model._validate()
@@ -36,7 +41,9 @@ def test_cn_model_ordered():
     freq_axis = np.linspace(113470.0, 113530.0, 1000)
     brightness = np.random.randn(1000)
     data = {"observation": SpecData(freq_axis, brightness, 1.0)}
-    model = CNModel(data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1)
+    model = CNModel(
+        data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1
+    )
     assert isinstance(model.mol_data, dict)
     model.add_priors(ordered=True)
     model.add_likelihood()
@@ -47,7 +54,9 @@ def test_cn_model_nonLTE():
     freq_axis = np.linspace(113470.0, 113530.0, 1000)
     brightness = np.random.randn(1000)
     data = {"observation": SpecData(freq_axis, brightness, 1.0)}
-    model = CNModel(data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1)
+    model = CNModel(
+        data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1
+    )
     assert isinstance(model.mol_data, dict)
     model.add_priors(assume_LTE=False, assume_CTEX=False)
     model.add_likelihood()
@@ -58,7 +67,9 @@ def test_cn_model_fix_Tkin():
     freq_axis = np.linspace(113470.0, 113530.0, 1000)
     brightness = np.random.randn(1000)
     data = {"observation": SpecData(freq_axis, brightness, 1.0)}
-    model = CNModel(data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1)
+    model = CNModel(
+        data, n_clouds=1, molecule="CN", mol_data=_MOL_DATA, baseline_degree=1
+    )
     assert isinstance(model.mol_data, dict)
     model.add_priors(fix_log10_Tkin=1.5)
     model.add_likelihood()
